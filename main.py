97508cc5f547129788880e49e8fd08e2277ffea1
Patrik Harmanos <harmanos.patrik@gmail.com>
2022-02-27 11:30:28 +0100

    added first vesrion of sip proxy

---
+++ main.py
@@ -0,0 +1,15 @@
+import socketserver
+import time
+import logging
+import sipproxy
+
+if __name__ == "__main__":
+    sipproxy.HOST = "192.168.0.164"
+    # sipproxy.HOST = input("Zadajte IP addresu: ")
+    logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', filename='proxy.log', level=logging.INFO,
+                        datefmt='%H:%M:%S')
+    logging.info(time.strftime("%a, %d %b %Y %H:%M:%S ", time.localtime()))
+    sipproxy.recordroute = "Record-Route: <sip:%s:%d;lr>" % (sipproxy.HOST, sipproxy.PORT)
+    sipproxy.topvia = "Via: SIP/2.0/UDP %s:%d" % (sipproxy.HOST, sipproxy.PORT)
+    server = socketserver.UDPServer((sipproxy.HOST, sipproxy.PORT), sipproxy.UDPHandler)
+    server.serve_forever()
